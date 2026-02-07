6462038e539a20d6935e0c29b5ee61c6737ad9c9
Sungsik Nam <jmyl@me.com>
2026-02-07 12:48:16 +0100

    Make Ngins handles many connections

--- Nginx/run.sh
+++ Nginx/run.sh
@@ -1,6 +1,17 @@
 #! /usr/bin/env bash
-podman run --rm -p 8080:8080 \
-    --name static-server \
-    -v "$PWD/../:/usr/share/nginx/html:ro,z" \
+# podman run --rm -p 8080:8080 \
+#     --name static-server \
+#     -v "$PWD/../:/usr/share/nginx/html:ro,z" \
+#     -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:ro,z" \
+#     nginx:alpine
+
+sudo podman rm -f static-server 2>/dev/null
+
+sudo podman run -d --name static-server \
+    -p 8080:8080 \
     -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:ro,z" \
     nginx:alpine
+
+sudo podman cp ../. static-server:/usr/share/nginx/html
+
+sudo podman exec static-server nginx -s reload
