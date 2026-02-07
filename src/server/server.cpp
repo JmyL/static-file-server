a04c7dbdcefd187b6567cfc81bcc551d30380e61
Sungsik Nam <jmyl@me.com>
2026-02-07 09:07:22 +0100

    Use namespace net = boost::asio on server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -14,36 +14,38 @@
 #include <thread>
 #include <utility>
 
-using boost::asio::ip::tcp;
+namespace net = boost::asio;
+using net::ip::tcp;
 using namespace std::string_view_literals;
 
 const int max_length = 1024;
 
 void session(tcp::socket sock) {
     try {
-        auto sbuf = boost::asio::streambuf(max_length);
+        auto sbuf = net::streambuf(max_length);
         for (;;) {
-            auto len = boost::asio::read_until(sock, sbuf, "\r\n\r\n");
+            auto len = net::read_until(sock, sbuf, "\r\n\r\n");
             std::cout << "Received " << len << " bytes\n";
             sbuf.consume(len);
 
-constexpr auto response = "HTTP/1.1 200 OK\r\n"
-                          "Server: nginx/1.29.4\r\n"
-                          "Date: Sat, 03 Jan 2026 15:22:53 GMT\r\n"
-                          "Content-Type: text/plain\r\n"
-                          "Content-Length: 0\r\n"
-                          "Last-Modified: Sun, 28 Dec 2025 17:25:28 GMT\r\n"
-                          "Connection: keep-alive\r\n"
-                          "ETag: \"69516808-4a8\"\r\n"
-                          "Accept-Ranges: bytes\r\n"
-                          "\r\n"sv;
+            constexpr auto response =
+                "HTTP/1.1 200 OK\r\n"
+                "Server: nginx/1.29.4\r\n"
+                "Date: Sat, 03 Jan 2026 15:22:53 GMT\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: 0\r\n"
+                "Last-Modified: Sun, 28 Dec 2025 17:25:28 GMT\r\n"
+                "Connection: keep-alive\r\n"
+                "ETag: \"69516808-4a8\"\r\n"
+                "Accept-Ranges: bytes\r\n"
+                "\r\n"sv;
 
-            auto write_len = boost::asio::write(
-                sock, boost::asio::buffer(response.data(), response.size()));
+            auto write_len =
+                net::write(sock, net::buffer(response.data(), response.size()));
             std::cout << "Sent " << write_len << " bytes\n";
         }
     } catch (boost::system::system_error &e) {
-        if (e.code() == boost::asio::error::eof) {
+        if (e.code() == net::error::eof) {
             std::cout << "Connection closed by peer.\n";
         } else {
             std::cerr << "Boost System Error in thread: " << e.what() << "\n";
@@ -53,7 +55,7 @@ constexpr auto response = "HTTP/1.1 200 OK\r\n"
     }
 }
 
-void server(boost::asio::io_context &io_context, unsigned short port) {
+void server(net::io_context &io_context, unsigned short port) {
     tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
     for (;;) {
         std::thread(session, a.accept()).detach();
@@ -67,7 +69,7 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
-        boost::asio::io_context io_context;
+        net::io_context io_context;
 
         server(io_context, std::atoi(argv[1]));
     } catch (std::exception &e) {
