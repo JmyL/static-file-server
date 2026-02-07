99cbbfc49da8d3aa40c6a4c29ca270cad515d002
Sungsik Nam <jmyl@me.com>
2026-02-07 13:00:11 +0100

    Made benchmark condition harsh, saw difference

--- k6/README.md
+++ k6/README.md
@@ -1,1 +1,11 @@
-You can run benchmark using `k6 run script.js`.
+You can run benchmark like below.
+
+Benchmarking Nginx:
+```bash
+PORT=8080 k6 run script.js
+```
+
+Benchmarking our server:
+```bash
+PORT=8080 k6 run script.js
+```
--- k6/script.js
+++ k6/script.js
@@ -2,13 +2,14 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-    vus: 10,
+    vus: 5000,
     duration: '30s',
 };
 
 export default function() {
-    let res = http.get('http://127.0.0.1:8081/files/README.md');
+    const port = __ENV.PORT || "8080";
+    let res = http.get(`http://127.0.0.1:${port}/files/README.md`);
 
     check(res, { "status is 200": (res) => res.status === 200 });
-    sleep(1);
+    sleep(0.01);
 }
