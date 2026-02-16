03ab0dad6f0e0c0b0a267020e2beaa8dacf373c7
Sungsik Nam <jmyl@me.com>
2026-02-16 19:24:37 +0100

    Use docker instead of podman

--- Nginx/run.sh
+++ Nginx/run.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
-# podman run --rm -p 8080:8080 \
+# docker run --rm -p 8080:8080 \
 #     --name static-server \
 #     -v "$PWD/../:/usr/share/nginx/html:ro,z" \
 #     -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:ro,z" \
 #     nginx:alpine
 
-sudo podman rm -f static-server 2>/dev/null
+docker rm -f static-server 2>/dev/null
 
-sudo podman run -d --name static-server \
+docker run -d --name static-server \
     -p 8080:8080 \
     -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:ro,z" \
     nginx:alpine
 
-sudo podman cp ../. static-server:/usr/share/nginx/html
+docker cp ../. static-server:/usr/share/nginx/html
 
-sudo podman exec static-server nginx -s reload
+docker exec static-server nginx -s reload
