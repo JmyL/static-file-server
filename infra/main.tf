ace4db99b83ca441df4460b643c654665fabc970
Sungsik Nam <jmyl@me.com>
2026-02-21 21:52:13 +0100

    Fix wrong output variable name

--- infra/main.tf
+++ infra/main.tf
@@ -67,9 +67,6 @@ resource "aws_instance" "k6_test" {
 output "test_server_public_ip" {
   value = aws_instance.test_server.public_ip
 }
-output "test_server_private_ip" {
-  value = aws_instance.test_server.private_ip
-}
 output "test_server_instance_id" {
   value = aws_instance.test_server.id
 }
