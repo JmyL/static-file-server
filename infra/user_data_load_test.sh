b15661a9e7faf96c790483bf650a907a1dde1921
Sungsik Nam <jmyl@me.com>
2026-02-22 21:05:55 +0100

    Install k6 correctly

--- infra/user_data_load_test.sh
+++ infra/user_data_load_test.sh
@@ -6,8 +6,8 @@ systemctl start docker
 
 snap install aws-cli --classic
 
-apt-get install -y curl
-curl -s https://dl.k6.io/key.gpg | apt-key add -
-echo "deb https://dl.k6.io/deb stable main" | tee /etc/apt/sources.list.d/k6.list
+gpg -k
+gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | tee /etc/apt/sources.list.d/k6.list
 apt-get update
 apt-get install k6
