e9dd9de8b79547997c9f83770ddcf1d9e431d806
Sungsik Nam <jmyl@me.com>
2026-03-01 15:28:25 +0100

    Pull docker images on user_data.sh

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -4,4 +4,7 @@ apt-get install -y docker.io
 systemctl enable docker
 systemctl start docker
 
+docker pull ghcr.io/jmyl/static-file-server/server:latest
+docker pull nginx:alpine
+
 snap install aws-cli --classic
